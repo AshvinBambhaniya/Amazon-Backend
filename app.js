c1c6bea42a2232605a1643a0e92b517a3271996c
Ashvin Bambhaniya <ashvinbambhaniya2003@gmail.com>
2023-03-30 12:19:27 +0530

    build react app

--- app.js
+++ app.js
@@ -2,30 +2,34 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5007;
-const cookieParser = require("cookie-parser");
-// const DefaultData = require("./defaultdata");
 require("./db/conn");
-const router = require("./routes/router");
+const cookieParser = require("cookie-parser");
+
+
 const products = require("./models/productsSchema");
-// const jwt = require("jsonwebtoken");
+// const DefaultData = require("./defaultdata");
 const cors = require("cors");
+const router = require("./routes/router");
+
+
+// const jwt = require("jsonwebtoken");
 
 
 // middleware
 app.use(express.json());
-app.use(cors());
 app.use(cookieParser(""));
-
+app.use(cors());
 app.use(router);
+
+const port = process.env.PORT || 5007;
 // app.get("/",(req,res)=>{
 //     res.send("your server is running");
 // });
 
 
-// if(process.env.NODE_ENV == "production"){
-    // app.use(express.static("client/build"));
-// }
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+}
 
 app.listen(port,()=>{
     console.log(`your server is running on port ${port} `);
